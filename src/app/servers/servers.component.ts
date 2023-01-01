f99edb84112820d7503b94c7f38a8418f204a00b
PariwatYn <pariwat.yimpayoon@itone.co.th>
2023-01-01 13:57:48 +0700

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,8 +8,9 @@ import { Component } from '@angular/core';
 
 })
 export class ServersComponent {
-    allowNewServer:boolean = false;
-
+    allowNewServer = false;
+    serverCreationStatus = 'No Server was created';
+    serverName = '';
     constructor() {
       setTimeout(() => {
         this.allowNewServer = true;
@@ -18,5 +19,13 @@ export class ServersComponent {
 
     ngOnInit(){
     }
+
+    onCreateServer(){
+      this.serverCreationStatus = 'Server was created'
+    }
+  onUpdatedServerName(event: Event){
+      console.log(event)
+      this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
 
