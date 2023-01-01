f46854793298f347cc4bb26d3f173c1f17952707
PariwatYn <pariwat.yimpayoon@itone.co.th>
2023-01-01 17:28:47 +0700

    ngfor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent {
     serverCreationStatus = 'No Server was created';
     serverName = 'TestName';
     serverCreated = false;
+    servers = ['Test00001','Test00002']
     constructor() {
       setTimeout(() => {
         this.allowNewServer = true;
@@ -23,11 +24,13 @@ export class ServersComponent {
 
     onCreateServer(){
       this.serverCreated = true;
+      this.servers.push(this.serverName);
       this.serverCreationStatus = 'Server was created Name is ' + this.serverName
     }
-  onUpdatedServerName(event: Event){
-      console.log(event)
-      this.serverName = (<HTMLInputElement>event.target).value;
-  }
+
+    onUpdatedServerName(event: Event){
+        console.log(event)
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
 }
 
