ab77703045c00e6b3de6f8e13c6ae491a901f46c
PariwatYn <pariwat.yimpayoon@itone.co.th>
2023-01-01 16:27:50 +0700

    ngif basic

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent {
     allowNewServer = false;
     serverCreationStatus = 'No Server was created';
     serverName = 'TestName';
+    serverCreated = false;
     constructor() {
       setTimeout(() => {
         this.allowNewServer = true;
@@ -21,6 +22,7 @@ export class ServersComponent {
     }
 
     onCreateServer(){
+      this.serverCreated = true;
       this.serverCreationStatus = 'Server was created Name is ' + this.serverName
     }
   onUpdatedServerName(event: Event){
