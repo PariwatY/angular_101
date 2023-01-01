4d9fc50396e78b41e78eeef08a43dc54310f1402
PariwatYn <pariwat.yimpayoon@itone.co.th>
2023-01-01 16:51:08 +0700

    ngstyle and ngclass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,6 +3,13 @@ import {Component} from "@angular/core";
 @Component({
   selector:'app-server',
   templateUrl: './server.component.html',
+  styles:[
+    `
+      .online {
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent{
   serverId:number = 10;
@@ -10,5 +17,13 @@ export class ServerComponent{
 
   getServerStatus(){
     return this.serverStatus;
+  }
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5? 'online':'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online'? 'green':'red';
   }
 }
